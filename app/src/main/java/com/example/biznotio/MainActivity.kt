528be07e8188ef4a08fb0502376b1e3b4cc18c02
See-in-doo99 <57790709+See-in-doo99@users.noreply.github.com>
2020-07-21 16:21:14 -0500

    Sign In fixed

--- app/src/main/java/com/example/biznotio/MainActivity.kt
+++ app/src/main/java/com/example/biznotio/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.example.biznotio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+
+import android.util.Log
+import android.widget.Toast
 import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.api.GoogleApiClient
 import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),GoogleApiClient.OnConnectionFailedListener{
+
+    companion object{
+        const val TAG= "MainActivity"
+        const val ANONYMOUS = "anonymus"
+    }
 
     private var userName: String? = null
     private var userPhotoUrl: String? = null
@@ -25,5 +36,27 @@ class MainActivity : AppCompatActivity() {
             .addApi(Auth.GOOGLE_SIGN_IN_API).build();
 
 
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+       // Log.d(TAG,msg: "onConnectionFailed $p0 ")
+        //Toast.makeText(context:this,"Google Play Services error",Toast.LENGTH_SHORT).show()
+         userName= ANONYMOUS
+        fireBaseAuth = FirebaseAuth.getInstance()
+        fireBaseUser=fireBaseAuth!!.currentUser
+
+        if(fireBaseUser==null)
+        {
+            val intent = Intent(this@MainActivity,SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        else{
+            userName=fireBaseUser!!.displayName
+            if(fireBaseUser!!.photoUrl!=null)
+            {
+                userPhotoUrl = fireBaseUser!!.photoUrl!!.toString()
+            }
+        }
     }
 }
