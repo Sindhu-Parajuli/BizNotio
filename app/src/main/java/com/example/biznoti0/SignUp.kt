8f2a5e155fdbd60b4fd3ffd8bc2bc1eff662b71d
See-in-doo99 <57790709+See-in-doo99@users.noreply.github.com>
2020-07-23 07:52:36 -0500

    Half way through sign-up

--- app/src/main/java/com/example/biznoti0/SignUp.kt
+++ app/src/main/java/com/example/biznoti0/SignUp.kt
@@ -1,10 +1,12 @@
 package com.example.biznoti0
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_sign_in.*
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_up.*
+
 
 class SignUp : AppCompatActivity() {
 
@@ -17,5 +19,57 @@ class SignUp : AppCompatActivity() {
 
         }
 
+        register.setOnClickListener {
+            Registration()
+        }
+
     }
+
+    private fun Registration()
+    {
+        val mfirebaseAuth: FirebaseAuth
+        mfirebaseAuth = FirebaseAuth.getInstance()
+        val Names = SignUpName.text.toString()
+        val emails = SignUpName.text.toString()
+        val passwords = SignUpName.text.toString()
+
+
+        if (Names.isEmpty())
+        {
+            Toast.makeText(this,"Name is must",Toast.LENGTH_LONG)
+        }
+
+        else if (emails.isEmpty())
+        {
+            Toast.makeText(this,"Email is must",Toast.LENGTH_LONG)
+        }
+        else if (passwords.isEmpty())
+        {
+            Toast.makeText(this,"Password is must",Toast.LENGTH_LONG)
+        }
+        else
+        {
+            mfirebaseAuth.createUserWithEmailAndPassword(emails, passwords)
+                .addOnCompleteListener{ task ->
+                    if (!task.isSuccessful) {
+                        Toast.makeText(this, "Sign up failed", Toast.LENGTH_SHORT).show();
+                    }
+
+                    else
+                    {
+                        store(Names,emails)
+                    }
+
+
+                }
+        }
+
+
+    }
+
+    private fun store(Names:String,emails:String)
+    {
+
+    }
+
 }
