31c1f56b7943bdd667bcdb62a74755cedd8bc6e3
See-in-doo99 <57790709+See-in-doo99@users.noreply.github.com>
2020-07-24 05:28:41 -0500

    Signup-SignIn fixed

--- app/src/main/java/com/example/biznoti0/SignUp.kt
+++ app/src/main/java/com/example/biznoti0/SignUp.kt
@@ -1,10 +1,13 @@
 package com.example.biznoti0
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 
@@ -15,7 +18,7 @@ class SignUp : AppCompatActivity() {
         setContentView(R.layout.activity_sign_up)
 
         signUpIn.setOnClickListener {
-            startActivity(Intent(this,SignInActivity::class.java))
+            startActivity(Intent(this, SignInActivity::class.java))
 
         }
 
@@ -25,39 +28,35 @@ class SignUp : AppCompatActivity() {
 
     }
 
-    private fun Registration()
-    {
-        val mfirebaseAuth: FirebaseAuth
-        mfirebaseAuth = FirebaseAuth.getInstance()
-        val Names = SignUpName.text.toString()
-        val emails = SignUpName.text.toString()
-        val passwords = SignUpName.text.toString()
+    private fun Registration() {
+        val mfirebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+        val names = SignUpName.text.toString()
+        val emails = SignUpEmail.text.toString()
+        val passwords = SignUpPassword.text.toString()
 
 
-        if (Names.isEmpty())
-        {
-            Toast.makeText(this,"Name is must",Toast.LENGTH_LONG)
-        }
+        if (names.isEmpty()) {
+            Toast.makeText(this, "Name is must", Toast.LENGTH_LONG)
+        } else if (emails.isEmpty()) {
+            Toast.makeText(this, "Email is must", Toast.LENGTH_LONG)
+        } else if (passwords.isEmpty()) {
+            Toast.makeText(this, "Password is must", Toast.LENGTH_LONG)
+        } else {
 
-        else if (emails.isEmpty())
-        {
-            Toast.makeText(this,"Email is must",Toast.LENGTH_LONG)
-        }
-        else if (passwords.isEmpty())
-        {
-            Toast.makeText(this,"Password is must",Toast.LENGTH_LONG)
-        }
-        else
-        {
+            val progressDialog = ProgressDialog(this@SignUp)
+            progressDialog.setTitle("SignUp")
+            progressDialog.setMessage("Sign up Process in Progress.....")
+            progressDialog.setCanceledOnTouchOutside(false)
+            progressDialog.show()
+
+
+
             mfirebaseAuth.createUserWithEmailAndPassword(emails, passwords)
-                .addOnCompleteListener{ task ->
+                .addOnCompleteListener { task ->
                     if (!task.isSuccessful) {
                         Toast.makeText(this, "Sign up failed", Toast.LENGTH_SHORT).show();
-                    }
-
-                    else
-                    {
-                        store(Names,emails)
+                    } else {
+                        store(names, emails, progressDialog)
                     }
 
 
@@ -67,9 +66,35 @@ class SignUp : AppCompatActivity() {
 
     }
 
-   private fun store(Names:String,emails:String)
-    {
+    private fun store(names: String, emails: String, progressDialog: ProgressDialog) {
+
+        val curruserId = FirebaseAuth.getInstance().currentUser!!.uid
+
+        val userreference: DatabaseReference = FirebaseDatabase.getInstance().reference.child(curruserId)
+
+        userreference.child("Name").setValue(names)
+        userreference.child("email").setValue(emails)
+        userreference.child("Image").setValue("gs://bitnoti0.appspot.com/user Info/profile.png")
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Sign up Success", Toast.LENGTH_SHORT).show();
+
+                    val intent = Intent(this@SignUp,MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+
+                }
+                else
+                {
+                    Toast.makeText(this, "Sign up failed", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
 
     }
 
+
 }
+
+
