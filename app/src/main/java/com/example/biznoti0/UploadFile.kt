6e1f2f79edb568507df700c9a245126129d54d6c
Lamia Chowdhury <67174767+Lamiachowdhury@users.noreply.github.com>
2020-07-24 14:31:58 -0500

    Update

--- app/src/main/java/com/example/biznoti0/UploadFile.kt
+++ app/src/main/java/com/example/biznoti0/UploadFile.kt
@@ -1,15 +1,31 @@
 package com.example.biznoti0
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
+import com.google.firebase.referencecode.storage.R
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageException
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
+import com.google.firebase.storage.ktx.storageMetadata
+import kotlinx.android.synthetic.main.activity_storage.imageView
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileInputStream
+
 import com.example.biznoti0.R
 
-class MainActivity : AppCompatActivity() {
+class UploadFile : AppCompatActivity() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,4 +35,36 @@ class MainActivity : AppCompatActivity() {
         return inflater.inflate(R.layout.upload_file, container, false)
     }
 
+    fun includesForUploadFiles() {
+        val storage = Firebase.storage
+        // Create a storage reference from our app
+        val storageRef = storage.reference
+
+        // Create a reference to "mountains.jpg"
+        val mountainsRef = storageRef.child("mountains.jpg")
+
+        // Create a reference to 'images/mountains.jpg'
+        val mountainImagesRef = storageRef.child("images/mountains.jpg")
+
+        // While the file names are the same, the references point to different files
+        mountainsRef.name == mountainImagesRef.name // true
+        mountainsRef.path == mountainImagesRef.path // false
+
+        //////////////
+        // [START upload_file]
+        var file = Uri.fromFile(File("path/to/images/rivers.jpg"))
+        val riversRef = storageRef.child("images/${file.lastPathSegment}")
+        uploadTask = riversRef.putFile(file)
+
+        // Register observers to listen for when the download is done or if it fails
+        uploadTask.addOnFailureListener {
+            // Handle unsuccessful uploads
+        }.addOnSuccessListener {
+            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+        }
+        // [END upload_file]
+
+        ///////////
+        // START upload_get_download_url
+    }
 }
