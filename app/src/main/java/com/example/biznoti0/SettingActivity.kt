b426e67504f7c9cf3ce37d70dc9dcb0398d75884
See-in-doo99 <57790709+See-in-doo99@users.noreply.github.com>
2020-07-27 22:06:04 -0500

    "FInal Update for Signup/Sign In"

--- app/src/main/java/com/example/biznoti0/SettingActivity.kt
+++ app/src/main/java/com/example/biznoti0/SettingActivity.kt
@@ -1,12 +1,47 @@
 package com.example.biznoti0
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_setting.*
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SettingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+        logout.setOnClickListener {
+            val progressDialog = ProgressDialog(this@SettingActivity)
+            progressDialog.setMessage("Logging out")
+            progressDialog.setCanceledOnTouchOutside(false)
+            progressDialog.show()
+
+
+            FirebaseAuth.getInstance().signOut();
+            startActivity(Intent(this, SignInActivity::class.java))
+
+        }
+
+        deletebutton.setOnClickListener {
+            val user =  FirebaseAuth.getInstance().currentUser!!
+
+            user.delete()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        startActivity(Intent(this, SignInActivity::class.java))
+                        Toast.makeText(this, "This Account has been deleted.", Toast.LENGTH_LONG).show()
+                    }
+                }
+
+        }
+
+
+
     }
 }
