fdf6f12383a9d89e30788ef7d5fdfc551e0e353d
jonathanpv <jonpad512@gmail.com>
2020-07-31 22:15:26 -0500

    add styling to chatlist fragment

--- app/src/main/java/Fragments/ChatListFragment.kt
+++ app/src/main/java/Fragments/ChatListFragment.kt
@@ -14,19 +14,11 @@ import com.example.biznoti0.ChatListDecoration
 
 import kotlinx.android.synthetic.main.fragment_chat_list.*
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 private lateinit var linearLayoutManager: LinearLayoutManager
-/**
- * A simple [Fragment] subclass.
- * Use the [ChatListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class ChatListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -50,15 +42,6 @@ class ChatListFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ChatListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             ChatListFragment().apply {
@@ -84,12 +67,10 @@ class ChatListFragment : Fragment() {
     }
 
     private fun initRecyclerView(){
-
-
         chat_list_recycler_view.apply {
             layoutManager = LinearLayoutManager(activity)
-            val chatListDecoration = ChatListDecoration(30)
-            addItemDecoration(chatListDecoration)
+//            val chatListDecoration = ChatListDecoration(30)
+//            addItemDecoration(chatListDecoration)
             chatListAdapter = ChatListRecyclerAdapter()
             adapter = chatListAdapter
         }
