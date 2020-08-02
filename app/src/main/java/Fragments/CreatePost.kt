a306dbd9874c1f9e16efc5b87bcb7d5ac47dd340
Lamia Chowdhury <67174767+Lamiachowdhury@users.noreply.github.com>
2020-08-02 00:43:16 -0500

    test commit

--- app/src/main/java/Fragments/CreatePost.kt
+++ app/src/main/java/Fragments/CreatePost.kt
@@ -24,7 +24,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [CreatePost.newInstance] factory method to
  * create an instance of this fragment.
  */
-
+// test commit
 class CreatePost : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
