69dd9c39254b7ad4acffefb62385a4746244f624
jonathanpv <jonpad512@gmail.com>
2020-08-01 22:31:28 -0500

    remove todo to prevent crash

--- app/src/main/java/Fragments/SearchFragment.kt
+++ app/src/main/java/Fragments/SearchFragment.kt
@@ -43,15 +43,15 @@ class SearchFragment : Fragment() {
         view.searchView.addTextChangedListener(object :TextWatcher
         {
             override fun afterTextChanged(p0: Editable?) {
-                TODO("Not yet implemented")
+
             }
 
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                TODO("Not yet implemented")
+
             }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                TODO("Not yet implemented")
+
             }
 
 
