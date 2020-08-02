faf3985e6282a9a145c947ca12dedcb45456edc0
See-in-doo99 <57790709+See-in-doo99@users.noreply.github.com>
2020-08-01 19:49:45 -0500

    "Trying to make search work, took messenger to home page"

--- app/src/main/java/Fragments/HomepageFragment.kt
+++ app/src/main/java/Fragments/HomepageFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.navigation.fragment.findNavController
 
 import com.example.biznoti0.R
 
@@ -19,6 +21,15 @@ class HomepageFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_homepage, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val imageButton = view.findViewById<ImageButton>(R.id.messenger)
+        imageButton?.setOnClickListener{
+            findNavController().navigate(R.id.chatListFragment, null)
+        }
     }
 
 }
