6f02f9d62fcdfda7650c4751ea72829623e7cc79
Lamia Chowdhury <67174767+Lamiachowdhury@users.noreply.github.com>
2020-08-02 00:34:58 -0500

    added space

--- app/src/main/java/Fragments/CreatePost.kt
+++ app/src/main/java/Fragments/CreatePost.kt
@@ -24,6 +24,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [CreatePost.newInstance] factory method to
  * create an instance of this fragment.
  */
+
 class CreatePost : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
